5f4bf972d3dbb3abf0bb1cef30302a50dd25ca99
goak ingoo <ingoo@goakui-MacBookAir.local>
2021-06-07 15:39:14 +0900

    modify file test.json -> test.js

---
+++ src/test.js
@@ -0,0 +1,51 @@
+data={
+    "menu1":{
+        "emoticon":[
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            },
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            },
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            },
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            },
+        ]
+        
+    },
+
+    "menu2":{
+        "emoticon":[
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            }
+        ]
+    },
+
+
+    "menu3":{
+        "emoticon":[
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            }
+        ]
+    },
+
+    "menu4":{
+        "emoticon":[
+            {
+                "content":"did you get a whoppa 🍔🍔",
+                "auth":"tecinpark"
+            }
+        ]
+    },
+}
